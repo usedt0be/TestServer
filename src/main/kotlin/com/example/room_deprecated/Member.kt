a74e28eacfe77e0c495d85d732bf31036a361052
Anton <anton.bets.00@mail.ru>
2025-03-12 15:17:29 +0600

    Added: Route without firebase authentication (for testing)
    Deleted: RoomController

--- src/main/kotlin/com/example/room_deprecated/Member.kt
+++
@@ -1,9 +0,0 @@
-package com.example.room_deprecated
-
-import io.ktor.websocket.*
-
-data class Member(
-    val username: String,
-    val sessionId: String,
-    val socket: WebSocketSession
-)
--- src/main/kotlin/com/example/room_deprecated/MemberAlreadyExistsException.kt
+++
@@ -1,5 +0,0 @@
-package com.example.room_deprecated
-
-class MemberAlreadyExistsException:Exception(
-    "There is already a member with that username in the room"
-)
